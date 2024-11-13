770af18b0a4e54bfb8e268b16007036ad9aa85cb
AbdallahShokry1999 <152289712+AbdallahShokry1999@users.noreply.github.com>
2024-11-13 15:31:50 +0200

    JTW_With_Password_verification_code

---
+++ src/main/java/com/example/Password_Verification/PasswordResetApplication.java
@@ -0,0 +1,13 @@
+package com.example.Password_Verification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordResetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordResetApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Password_Verification/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.Password_Verification.service;
+
+import com.example.Password_Verification.model.User;
+import com.example.Password_Verification.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository, EmailService emailService) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+}
